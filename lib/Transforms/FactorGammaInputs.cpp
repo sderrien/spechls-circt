ddd1001a8d2dbce6cb8195ba7998520241cb72ac
Steven Derrien <sderrien@irisa.fr>
2024-01-19 18:07:15 +0100

    First draft for FactorGammaInput (unfinished)

--- lib/Transforms/FactorGammaInputs.cpp
+++ lib/Transforms/FactorGammaInputs.cpp
@@ -31,99 +31,152 @@ namespace SpecHLS {
 struct FactorGammaInputsPattern : OpRewritePattern<GammaOp> {
 
   using OpRewritePattern<GammaOp>::OpRewritePattern;
+private:
+  bool checkmatch(GammaOp op, int i, int k, SmallVector<int32_t> matches) const {
+    Value va = op.getOperand(i);
+    Value vb = op.getOperand(k);
 
-  LogicalResult matchAndRewrite(GammaOp op,
-                                PatternRewriter &rewriter) const override {
-
-    // llvm::errs() << "analyzing  " << op << " \n";
-    auto nbOuterInputs = op.getInputs().size();
-    for (int i = 0; i < op.getInputs().size(); i++) {
-      //      llvm::errs() << "\tinput  value " << op.getInputs()[i] << " \n";
-      auto input = op.getInputs()[i].getDefiningOp();
-      if (input != NULL) {
-
-        //     llvm::errs() << "\tinput op " << input << " \n";
-
-        if (llvm::isa<SpecHLS::GammaOp>(input)) {
-
-          auto innerGamma = cast<SpecHLS::GammaOp>(input);
-          //          llvm::errs() << "Found nested gamma \n";
-          //          llvm::errs() << "\t inner " << innerGamma << "\n";
-          //          llvm::errs() << "\t outer " << op << "\n";
-
-          auto users = innerGamma->getUsers();
-          auto nbusers = std::distance(users.begin(), users.end());
-
-          if (nbusers == 1) {
-            //            llvm::errs() << "Found nested gamma \n";
-            //            llvm::errs() << "\t inner " << innerGamma << "\n";
-            //            llvm::errs() << "\t outer " << op << "\n";
-
-            auto nbInnerInputs = innerGamma.getInputs().size();
-            int newDepth =
-                int(ceil(log(nbOuterInputs + nbInnerInputs) / log(2)));
-            //  Value* muxOperands = new Value[cwidth * (cwidth+1)];
-            Operation::operand_range in = innerGamma.getInputs();
-            SmallVector<Value, 8> muxOperands;
-            // muxOperands.append(innerGamma.getInputs())
-
-            for (int pos = 0; pos < nbInnerInputs; pos++) {
-              muxOperands.push_back(innerGamma.getInputs()[pos]);
-            }
-            for (int pos = 0; pos < nbOuterInputs; pos++) {
-              if (pos != i) {
-                muxOperands.push_back(op.getInputs()[pos]);
+    if (va.getType() == vb.getType()) {
+      auto a  = va.getDefiningOp();
+      auto b  = vb.getDefiningOp();
+      if (b != NULL) {
+        if (a->getName() == b->getName()) {
+          if (a->getDialect() == b->getDialect()) {
+            if (a->getNumOperands() == b->getNumOperands()) {
+              if (b->getNumResults()==1 && b->getResult(0).hasOneUse()) {
+                llvm::outs() << "Found match " << *a  << "and "<< *b << "\n";
+                matches.push_back(k);
+                return true;
               }
             }
-            auto controlType = rewriter.getIntegerType(
-                op.getSelect().getType().getIntOrFloatBitWidth() +
-                innerGamma.getSelect().getType().getIntOrFloatBitWidth());
-            auto concatOp = rewriter.create<comb::ConcatOp>(
-                op.getLoc(), controlType,
-                ValueRange({op.getSelect(), innerGamma.getSelect()}));
-
-            ArrayAttr tab;
-            SmallVector<int, 1024> content;
-            int offset = 0;
-            int index = 0;
-            int innerPow2Inputs =
-                1 << innerGamma.getSelect().getType().getIntOrFloatBitWidth();
-            int outerPow2Inputs =
-                1 << op.getSelect().getType().getIntOrFloatBitWidth();
-            for (int o = 0; o < outerPow2Inputs; o++) {
-
-              for (int inner = 0; inner < innerPow2Inputs; inner++) {
-                // llvm::errs() << "\t\t- LUT["<< inner << "," << o << "->" <<
-                // index << "] =" << offset << "\n";
-                content.push_back(offset);
-                index++;
-                if (o == i)
-                  offset++;
-              }
-              if (o != i)
-                offset++;
-            }
-
-            int depth =
-                int(ceil(log(nbInnerInputs + nbOuterInputs - 1) / log(2)));
-            mlir::Type addrType = rewriter.getIntegerType(depth);
-
-            auto lutSelect = rewriter.create<LookUpTableOp>(
-                op.getLoc(), addrType, concatOp.getResult(),
-                rewriter.getI32ArrayAttr(content));
-
-            rewriter.replaceOpWithNewOp<GammaOp>(op, op->getResultTypes(),
-                                                 lutSelect.getResult(),
-                                                 ValueRange(muxOperands));
-
-            lutSelect->getParentOfType<mlir::ModuleOp>().dump();
-            // rewriter.eraseOp(innerGamma);
-            return success();
           }
         }
       }
     }
+    return false;
+  }
 
+public:
+  LogicalResult matchAndRewrite(GammaOp op,
+                                PatternRewriter &rewriter) const override {
+
+    // llvm::errs() << "analyzing  " << op << " \n";
+    for (int i = 1; i < op.getInputs().size(); i++) {
+      auto rootValue = op.getInputs()[i];
+      auto root = rootValue.getDefiningOp();
+      if (root!=NULL) {
+        if (root->getNumResults()==1) {
+          if (root->getResult(0).hasOneUse()) {
+            SmallVector<int32_t> matches;
+            matches.push_back(i);
+            for (int k = i + 1; i < op->getNumOperands(); i++) {
+              checkmatch(op, i, k, matches);
+            }
+            if (matches.size()>1) {
+              llvm::outs() << "match set :\n";
+
+              for (auto m : matches) {
+                llvm::outs() << "   - "<< m << "\n";
+              }
+
+              /*
+               * creates a LUT for reindexing innergamme inputs
+               */
+              SmallVector<int> lutcontent;
+              int pos = 0;
+              for (int k= 0; k<root->getNumOperands(); k++) {
+                if (std::count_if(matches.begin(), matches.end(), [&](const auto &item) {
+                      return (k==item);
+                    })) {
+                  lutcontent.push_back(pos++);
+                } else {
+                  lutcontent.push_back(0);
+                }
+              }
+              auto innerLut = rewriter.create<SpecHLS::LookUpTableOp>(op->getLoc(),op->getResultTypes(),op.getOperand(0), rewriter.getI32ArrayAttr(lutcontent));
+
+
+              /*
+               * creates a LUT for reindexing outergamme inputs
+               */
+              SmallVector<int> outerLutContent;
+              pos = 0;
+              for (int k= 0; k<=i; k++) {
+                outerLutContent.push_back(k);
+              }
+              for (int k= i+1; k<op->getNumOperands(); k++) {
+                if (std::count_if(matches.begin(), matches.end(), [&](const auto &item) {
+                      return (k==item);
+                    })) {
+                  outerLutContent.push_back(i);
+                } else {
+                  outerLutContent.push_back(pos++);
+                }
+              }
+
+              llvm::outs() << "Outer gamma "<< op<< "reindexing  \n";
+              for (int k= 0; k<op->getNumOperands(); k++) {
+                llvm::outs() << " - input " << k <<" reindexed to "<< outerLutContent[k] <<" \n";
+              }
+
+              auto outerLut = rewriter.create<SpecHLS::LookUpTableOp>(op->getLoc(),op->getResultTypes(),op.getOperand(0), rewriter.getI32ArrayAttr(outerLutContent));
+
+              /*
+               * Create a LUT for reindexing inner gamma inputs.
+               *
+               */
+
+              SmallVector<Value> newGammas;
+              for (int j=0;j<root->getNumOperands();j++) {
+                SmallVector<Value> args;
+                llvm::outs() << "extracting all " << j <<"th args in matched ops \n";
+
+                for (auto mid : matches) {
+                  auto matchedValue = op.getOperand(mid);
+                  if (!matchedValue) {
+                    auto matchedOp = matchedValue.getDefiningOp();
+                    if (!matchedOp) {
+                      llvm::outs() << " analyzing match "<< matchedOp <<" at offset  "<< j << "\n";
+                      auto matchedArgValue = matchedOp->getOperand(j);
+                      if (!matchedArgValue) {
+                        llvm::outs() << "     extracting value "<< matchedArgValue << " \n";
+                        args.push_back(matchedArgValue);
+                      } else {
+                        llvm::errs() << "No valid arrgValue at offset " << j << "\n";
+                        return failure();
+                      }
+                    } else {
+                      llvm::errs() << "No defining op for value "<< matchedValue << " at offset " << mid << "\n";
+                      return failure();
+                    }
+                  } else {
+                    llvm::errs() << "Invalid value at " << mid << "\n";
+                    return failure();
+                  }
+                }
+                auto gamma = rewriter.create<SpecHLS::GammaOp>(op->getLoc(),op->getResultTypes(), op.getName(),innerLut->getResult(0),args);
+                llvm::outs() << "creating inner gamma "<< gamma<<" at offset  "<< j << "\n";
+                newGammas.push_back(gamma);
+              }
+
+              assert(newGammas.size()==root->getNumOperands());
+              for (int j=0;j<root->getNumOperands();j++) {
+                llvm::outs() << "replace arg["<<j <<"]="<< root->getOperand(j) <<" by  "<< newGammas[j] << "\n";
+                root->setOperand(j,newGammas[j]);
+              }
+              llvm::outs() << "Root op is now "<< root << "\n";
+
+              for (auto mid : matches) {
+                llvm::outs() << "change value  "<< op->getOperand(mid) << " by "<< rootValue << "\n";
+                op->setOperand(mid,rootValue);
+              }
+              llvm::outs() << "After rewiring outer gamma "<< op << "\n";
+              return success();
+            }
+          }
+        }
+      }
+    }
     return failure();
   }
 };
