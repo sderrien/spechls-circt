8ebf358f868cff05a67b5a9c5a944415d997c79c
Steven Derrien <sderrien@irisa.fr>
2024-02-22 17:05:53 +0100

    - Cleanup

--- lib/Transforms/SpecHLSToComb/EncoderToComb.cpp
+++ lib/Transforms/SpecHLSToComb/EncoderToComb.cpp
@@ -40,79 +40,31 @@ struct EncoderToCombOpConversion : OpConversionPattern<EncoderOp> {
     return false;
   }
 
-  LogicalResult
-  matchAndRewrite(EncoderOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
+  LogicalResult matchAndRewrite(EncoderOp op, OpAdaptor adaptor, ConversionPatternRewriter &rewriter) const override {
 
     uint32_t uiwidth = op.getData().getType().getWidth();
-
-    APInt width = APInt(32,uiwidth);
+    auto loc = op.getLoc();
+    APInt width = APInt(32, uiwidth);
     int bw = width.exactLogBase2();
+    auto resType= op.getResult().getType();
 
-    auto concatInput = rewriter.create<ConcatOp>(op.getLoc(), op->getOperands());
+    auto concatInput = rewriter.create<ConcatOp>(loc, op->getOperands());
 
-    llvm::outs() << "width " << bw << "\n";
-    if (uiwidth<=8) {
+    if (uiwidth <= 8) {
       SmallVector<Attribute> table;
-      for (auto k= 0;k<(1<<uiwidth);k++) {
-        auto val = APInt(bw,k, false);
+      for (auto k = 0; k < (1 << uiwidth); k++) {
+        auto val = APInt(bw, k, false);
         auto ldz = val.countLeadingZeros();
         table.push_back(rewriter.getI32IntegerAttr(ldz));
       }
       auto content = rewriter.getArrayAttr(ArrayRef(table));
-      auto selBit = rewriter.create<LookUpTableOp>(op.getLoc(), op.getResult().getType(),concatInput->getResults()[0], content);
+      auto value = concatInput->getResults()[0];
+      auto selBit = rewriter.create<LookUpTableOp>(loc, resType, value, content);
     }
 
-    auto extractBit = rewriter.create<ExtractOp>(op.getLoc(), op.getData(), 0,width.getZExtValue()/2);
+    auto extractBit = rewriter.create<ExtractOp>(loc, op.getData(), 0, width.getZExtValue() / 2);
 
-
-    //    Value *muxOperands = new Value[cwidth * (cwidth + 1)];
-//    // S//mallVector<Value,SPEC_EncoderOp_MAXOPERANDS> muxOperands;
-//
-//    for (int k = 0; k < cwidth; k += 1) {
-//      if (k < width) {
-//        muxOperands[k] = op.getInputs()[k];
-//      } else {
-//        muxOperands[k] = op.getInputs()[width - 1];
-//      }
-//      // llvm::errs() << "\t-mux[" << k << "," << 0 << "] = "<< muxOperands[k]
-//      // <<"\n" ;
-//    }
-//    while (cwidth > 1) {
-//      // llvm::errs()  << "depth = " << depth << " -> extract "<< selBit <<"\n";
-//      for (int k = 0; k < cwidth; k += 2) {
-//        // llvm::errs()  << "  k=  " << k <<"\n";
-//        int offset = (width * depth);
-//        Value opa = muxOperands[k + offset];
-//        Value opb = NULL;
-//        // llvm::errs() << "opa = mux[" << k << "," << depth << "] = "<< opa
-//        // <<"\n" ;
-//
-//        if (k + 1 < cwidth) {
-//          opb = muxOperands[k + 1 + offset];
-//          // llvm::errs() << "opb = mux[" << k+1 << "," << depth << "] = "<< opb
-//          // <<"\n" ;
-//        } else {
-//          opb = muxOperands[k + offset];
-//          // llvm::errs() << "opb = mux[" << k << "," << depth << "]= "<< opb
-//          // <<"\n" ; ;
-//        }
-//        if (opa || opb) {
-//          // llvm::errs() << " null";
-//        }
-//        auto mux = rewriter.create<MuxOp>(op.getLoc(), selBit, opa, opb);
-//        // llvm::errs() << "mux[" << k/2 << "," << depth+1 << "] = " ;
-//        // llvm::errs() << mux;
-//        muxOperands[k / 2 + offset + width] = mux;
-//      }
-//      cwidth = cwidth / 2;
-//      depth++;
-//    }
-//    // llvm::errs() << "result is " <<  muxOperands[width*depth] << "\n" ;
-//    rewriter.replaceOp(op, muxOperands[width * depth]);
-     return success();
-
-    return failure();
+    return success();
   }
 };
 
