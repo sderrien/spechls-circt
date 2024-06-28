5e0c071c85a1493f4a7255be28afabea55900a69
Steven Derrien <sderrien@irisa.fr>
2024-06-28 14:14:47 +0200

    - Fix I/O issues

--- include/Transforms/SpecHLSConversion.h
+++ include/Transforms/SpecHLSConversion.h
@@ -37,4 +37,52 @@ struct RollbackToCombConversion : OpRewritePattern<RollbackOp> {
                                 PatternRewriter &rewriter) const override;
 };
 
+struct DelayOpToShiftRegOpConversion : OpRewritePattern<DelayOp> {
+  using OpRewritePattern<DelayOp>::OpRewritePattern;
+  DelayOpToShiftRegOpConversion(MLIRContext *context,Value *clock, Value *reset);
+  Value* clock = NULL;
+  Value* reset = NULL;
+  LogicalResult matchAndRewrite(DelayOp op, PatternRewriter &rewriter) const override;
+};
+
+struct AlphaOpToHLWriteConversion : OpRewritePattern<AlphaOp> {
+  using OpRewritePattern<AlphaOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(AlphaOp op,
+                                PatternRewriter &rewriter) const override;
+  AlphaOpToHLWriteConversion(MLIRContext *context,Value *clock, Value *reset,llvm::DenseMap<MuOp,circt::seq::HLMemOp> memMap);
+  Value* clock = NULL;
+  Value* reset = NULL;
+  llvm::DenseMap<MuOp,circt::seq::HLMemOp> memMap;
+};
+
+struct ArrayReadOpToHLReadConversion : OpRewritePattern<ArrayReadOp> {
+  using OpRewritePattern<ArrayReadOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(ArrayReadOp op,
+                                PatternRewriter &rewriter) const override;
+  ArrayReadOpToHLReadConversion(MLIRContext *context,Value *clock, Value *reset,llvm::DenseMap<MuOp,circt::seq::HLMemOp> memMap);
+  Value* clock = NULL;
+  Value* reset = NULL;
+  llvm::DenseMap<MuOp,circt::seq::HLMemOp> memMap;
+};
+
+struct MuOpToRegConversion : OpRewritePattern<MuOp> {
+  using OpRewritePattern<MuOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(MuOp op,
+                                PatternRewriter &rewriter) const override;
+  MuOpToRegConversion(MLIRContext *context,Value *clock, Value *reset,llvm::DenseMap<MuOp,circt::seq::HLMemOp> &memMap);
+  Value* clock = NULL;
+  Value* reset = NULL;
+  llvm::DenseMap<MuOp,circt::seq::HLMemOp> *memMap;
+};
+
+struct RollbackOpToSeqConversion : OpRewritePattern<RollbackOp> {
+  using OpRewritePattern<RollbackOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(RollbackOp op,
+                                PatternRewriter &rewriter) const override;
+  RollbackOpToSeqConversion(MLIRContext *context,Value *clock, Value *reset);
+  Value* clock = NULL;
+  Value* reset = NULL;
+};
+
+
 #endif // SPECHLS_DIALECT_SPECHLSCONVERSION_H
